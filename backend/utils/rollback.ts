0df02fc671e2dee6fe01631db438ab791435f67e
Amy Hoang <my.hoang@tuni.fi>
2023-08-05 13:56:35 +0300

    roll back to prev version to make the database from start

--- backend/utils/rollback.ts
+++
@@ -1,9 +0,0 @@
-import { rollbackMigration } from './db';
-
-rollbackMigration()
-  .then(() => {
-    console.log('Rollback completed successfully.');
-  })
-  .catch((error) => {
-    console.error('Error during rollback:', error);
-  });
