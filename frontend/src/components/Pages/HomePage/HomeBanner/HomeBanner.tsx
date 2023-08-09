c8935ea608ca009cf6de3fbd9147334815edf6f8
Amy Hoang <my.hoang@tuni.fi>
2023-08-09 16:10:49 +0300

    explore ai button

--- frontend/src/components/Pages/HomePage/HomeBanner/HomeBanner.tsx
+++ frontend/src/components/Pages/HomePage/HomeBanner/HomeBanner.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
+
 const HomeBanner = () => {
+    const navigate = useNavigate(); // Initialize the navigate function
+
+    const handleExploreAI = () => {
+        navigate('/ais'); // Navigate to the AI Tools page
+    };
+
     return (
         <div>
             <h2>UNLOCK THE POTENTIAL OF ARTIFICIAL INTELLIGENCE
@@ -8,9 +16,10 @@ const HomeBanner = () => {
                 where we empower businesses to harness the full potential of Artificial Intelligence 
                 and propel themselves into the future.
             </p>
-            <button>Explore AI</button>
+            <button onClick={handleExploreAI}>Explore AI</button>
             <button>Rate AI</button>
         </div>
-    )
-}
+    );
+};
+
 export default HomeBanner;
